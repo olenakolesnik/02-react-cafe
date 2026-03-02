9c5a35b99ba1f6b756967e031e259134c383724c
Olena Kolesnik <lelya6350@gmail.com>
2026-03-02 10:40:32 +0200

    fixed error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import { useState } from 'react';
-import { Votes, VoteType } from '../../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
